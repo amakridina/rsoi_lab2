a618155b986b0b330565d638fb3f7a623d2883c0
amakridina <annet1543@gmail.com>
2015-11-20 00:58:35 +0300

    получаю данные, надо теперь в базу их записывать

--- API.py
+++ API.py
@@ -10,14 +10,23 @@ def test():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return 'POST LOGIN';
+        username = request.form['username']
+        password = request.form['password']
+        return 'WELCOME TO MUSIC LIB'
     return app.send_static_file('auth.html')
 
 @app.route("/register", methods=['GET', 'POST'])
 def registerForm():
     if request.method == 'POST':
-        return 'POST REGISTER';
+        username = request.form['username']
+        first_name = request.form['first_nsme']
+        last_name = request.form['last_name']
+        tel = request.form['tel']
+        email = request.form['email']
+        password = request.form['password']
+        return 'Are you registered'
     return app.send_static_file('register.html')
+
 
 if __name__ == "__main__":
  app.run(debug=True, port=27010)
