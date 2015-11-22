02ec03e1582678170172e40c12563af370a7c4fa
amakridina <annet1543@gmail.com>
2015-11-23 00:42:43 +0300

    небольшая замена формы, чтоб отображалось сообщение об ошибке попытки начать делать методы me, status и oauth2

--- API.py
+++ API.py
@@ -1,71 +1,103 @@
-from flask import Flask,redirect,request
+# -*- coding: cp1251 -*-
+from flask import Flask,redirect,request,render_template
 import requests
 import pyodbc
 
-app = Flask(__name__)
+app = Flask(__name__, static_path='')
 
 @app.route("/")
 def test():
     return "Test";
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        connStr = (
+#подключение к БД
+def connectDB():
+    connStr = (
             r'DRIVER={SQL Server};' +
             r'SERVER=(local)\SQLEXPRESS;' +
             r'Database=rsoi;' +
             r'UID=rsoi;PWD=Aa123456'
             )
+    return pyodbc.connect(connStr)
 
-        db = pyodbc.connect(connStr)
+#авторизация
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    errorInfo = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db = connectDB()
         select_str =("select count(*) as num from Users"+
                      " where UserName = '%s' and Password = '%s'"
                      % (username, password))
         cursor = db.execute(select_str)
         row = cursor.fetchone()
         if row.num == 0:
-            return ("Incorrect login or password")
-        return 'WELCOME TO MUSIC LIB'
-    return app.send_static_file('auth.html')
+            errorInfo = "Incorrect login or password"
+        else:
+            return 'WELCOME TO MUSIC LIB'
+    return render_template('auth.html', errorInfo=errorInfo)
 
-@app.route("/register", methods=['GET', 'POST'])
-def registerForm():
-    if request.method == 'POST':
-        username = request.form['username']
-        first_name = request.form['fname']
-        last_name = request.form['lname']
-        tel = request.form['tel']
-        email = request.form['email']
-        password = request.form['password']
 
-        connStr = (
-            r'DRIVER={SQL Server};' +
-            r'SERVER=(local)\SQLEXPRESS;' +
-            r'Database=rsoi;' +
-            r'UID=rsoi;PWD=Aa123456'
-            )
-        db = pyodbc.connect(connStr)
-
-        select_str = ("select count(*) as num from Users where UserName = '%s'"% username)
-        cursor = db.execute(select_str)
-        row = cursor.fetchone()
-        if row.num > 0:
-            return ("User '%s' already exists!"% username)
-        
-        insert_str  = ("insert into Users"+
+#регистрация
+def addUser(db, request):
+    username = request.form['username']
+    first_name = request.form['fname']
+    last_name = request.form['lname']
+    tel = request.form['tel']
+    email = request.form['email']
+    password = request.form['password']
+    insert_str  = ("insert into Users"+
                        " (UserName, FirstName, LastName, Telephone, Email, Password)"+
                        " values ('%s','%s','%s','%s','%s','%s' )"
                        % (username, first_name, last_name, tel, email, password))
-        db.execute(insert_str)
-        db.commit()
-        
-        return 'Are you registered'
-    return app.send_static_file('register.html')
+    db.execute(insert_str)
+    db.commit()
+    
+@app.route("/register", methods=['GET', 'POST'])
+def registerForm():
+    errorInfo = ''
+    if request.method == 'POST':
+        db = connectDB()
+        username = request.form['username']
 
+        select_str = ("select count(*) as num from Users where UserName = '%s'"
+                      % request.form['username'])
+        cursor = db.execute(select_str)
+        row = cursor.fetchone()
+        
+        if row.num > 0:
+            errorInfo = ("User '%s' already exists!"% username)
+        else:
+            addUser(db, request)
+            return 'Are you registered'
+    return render_template('register.html', errorInfo=errorInfo)
+
+
+#получение кода
+@app.route("/api/oauth2/authorize", methods=['GET'])
+def userCode():
+    #in: appId, redirectUri
+    #out: code
+    
+    return "code"
+
+# получение access_token
+@app.route("/api/oauth2/token", methods=['POST'])
+def access_token():
+    return None
+
+#метод получения статуса пользователя авторизован/не авторизован
+#@app.route("/api/status")
+#def myStatus:
+    
+
+#метод получения информации о пользователе
+@app.route("/api/me", methods=["GET"])
+def userInfo():
+    userInfo = "kj"
+    return userInfo
 
 if __name__ == "__main__":
  app.run(debug=True, port=27010)
