20f1e109c8b56ff4f0b650d07da46a7e8919d6c4
amakridina <annet1543@gmail.com>
2015-11-22 14:40:30 +0300

    При регистрации данные уже заносятся в БД без всяких проверок пока

--- API.py
+++ API.py
@@ -1,5 +1,6 @@
-from flask import Flask,redirect,request,session
+from flask import Flask,redirect,request
 import requests
+import pyodbc
 
 app = Flask(__name__)
 
@@ -12,6 +13,20 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+
+        connStr = (
+            r'DRIVER={SQL Server};' +
+            r'SERVER=(local)\SQLEXPRESS;' +
+            r'Database=rsoi;' +
+            r'UID=rsoi;PWD=Aa123456'
+            )
+
+        db = pyodbc.connect(connStr)
+        select_str = 'select count(*) as num from Users'
+        cursor = db.execute(select_str)
+        row = cursor.fetchone()
+        if row:
+            return str(row.num)
         return 'WELCOME TO MUSIC LIB'
     return app.send_static_file('auth.html')
 
@@ -19,11 +34,26 @@ def login():
 def registerForm():
     if request.method == 'POST':
         username = request.form['username']
-        first_name = request.form['first_nsme']
-        last_name = request.form['last_name']
+        first_name = request.form['fname']
+        last_name = request.form['lname']
         tel = request.form['tel']
         email = request.form['email']
         password = request.form['password']
+
+        connStr = (
+            r'DRIVER={SQL Server};' +
+            r'SERVER=(local)\SQLEXPRESS;' +
+            r'Database=rsoi;' +
+            r'UID=rsoi;PWD=Aa123456'
+            )
+
+        db = pyodbc.connect(connStr)
+        insert_str  = ("insert into Users"+
+                       " (UserName, FirstName, LastName, Telephone, Email, Password)"+
+                       " values ('%s','%s','%s','%s','%s','%s' )"
+                       % (username, first_name, last_name, tel, email, password))
+        db.execute(insert_str)
+        db.commit()
         return 'Are you registered'
     return app.send_static_file('register.html')
 
